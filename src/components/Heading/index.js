1c399f61a5cd4bcdb96a0ef606fe690bca1fd514
Daniel Needham <dan@Daniels-MacBook-Pro.local>
2017-11-29 08:32:30 +0000

    Add heading classes

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import injectSheet from 'react-jss';
+import classNames from 'classnames';
+
 import styles from './styles';
 
 const Heading = ({ children, classes, level }) => {
   const TagName = `h${level}`;
+  const headingClasses = classNames(
+    classes.heading,
+    classes[`h${level}`
+  ]);
 
   return (
-    <TagName>{children}</TagName>
+    <TagName className={headingClasses}>{children}</TagName>
   );
 };
 
@@ -16,6 +22,7 @@ Heading.defaultProps = {
 };
 
 Heading.propTypes = {
+  children: PropTypes.node.isRequired,
   classes: PropTypes.object.isRequired,
   level: PropTypes.oneOf([1,2,3]),
 };
--- src/components/Heading/index.spec.js
+++ src/components/Heading/index.spec.js
@@ -6,7 +6,7 @@ import Heading from './';
 describe('Heading', () => {
   describe('level prop', () => {
     [1,2,3].map(level => {
-      const component = mount(<Heading level={level} />);
+      const component = mount(<Heading level={level}>Heading</Heading>);
       const tagName = `h${level}`;
 
       test(`renders ${tagName} tag for ${level}`, () => {
