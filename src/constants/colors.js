156c35bfb74ca751dfbdb6adcd9a9caf941226c2
dndhm <dneedham@gmail.com>
2018-03-12 08:26:31 +0000

    Add colors constants

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export const PROJECT_SLATE = 'project-slate';
+export const WHITE = 'white';
+export const MID_GREY = 'mid-grey';
+
+export default {
+	[PROJECT_SLATE]: '#191919',
+	[WHITE]: '#fff',
+	[MID_GREY]: '#888',
+};
