227fe92030ba95ad71da304dbf3a56fa4089f440
dndhm <dneedham@gmail.com>
2018-03-14 20:36:55 +0000

    Project layout, styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,30 +6,50 @@ import colors, {
 	PROJECT_SLATE,
 	WHITE,
 } from '../../constants/colors';
+
+import Heading from '../Heading';
 import Paragraph from '../Paragraph';
+import ProjectTags, { tags } from '../ProjectTags';
 
 const styles = {
 	container: {
 		background: colors[PROJECT_SLATE],
+		'text-align': 'center',
 	},
-	heading: {
-		color: colors[WHITE],
-		'font-size': '12px',
-		padding: '20px 0 10px',
-		'margin-bottom': '10px',
+	content: {
+		padding: '10px',
+	},
+	description: {
+		margin: '0 auto 10px',
+		width: '80%',
 	},
 };
 
-const Project = ({ classes, description, title }) => (
+const Project = ({
+	activeTag,
+	classes,
+	description,
+	tags,
+	title,
+}) => (
 	<div className={classes.container}>
-		<h3 className={classes.heading}>{title}</h3>
-		<Paragraph>{description}</Paragraph>
+		<div className={classes.content}>
+			<Heading color={WHITE} level={3}>{title}</Heading>
+			<ProjectTags active={activeTag} tags={tags} />
+			<div className={classes.description}>
+				<Paragraph>{description}</Paragraph>
+			</div>
+		</div>
 	</div>
 );
 
 Project.propTypes = {
+	activeTag: propTypes.oneOf(tags),
 	classes: propTypes.object,
 	description: propTypes.node.isRequired,
+	tags: propTypes.arrayOf(
+		propTypes.oneOf(tags)
+	).isRequired,
 	title: propTypes.string.isRequired,
 };
 
